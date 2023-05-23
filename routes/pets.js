a3b26987660952dbbc48f65cf1652d959fb729e0
Josh Faigan <djjoker13@gmail.com>
2023-05-22 22:15:27 -0400

    implemented search pagination

--- routes/pets.js
+++ routes/pets.js
@@ -7,24 +7,21 @@ module.exports = (app) => {
   // INDEX PET => index.js
 
   // SEARCH PET
-  // SEARCH PET
-  // SEARCH PET
   app.get('/search', (req, res) => {
     const term = new RegExp(req.query.term, 'i');
   
-    Pet.find({ $or: [{ 'name': term }, { 'species': term }] }).exec((err, pets) => {
-      res.render('pets-index', { pets: pets });
-    });
+    const page = req.query.page || 1
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
+      });
   });
-  // app.get('/search', (req, res) => {
-  //   Pet.find({ $text: { $search: req.query.term } })
-  //     .then((pets) => {
-  //       res.render('pets-index', { pets: pets });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // });
 
   // NEW PET
   app.get('/pets/new', (req, res) => {
